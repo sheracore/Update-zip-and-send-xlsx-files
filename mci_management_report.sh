3ce69389997aacc542bfefc9fe95ea1cc8572504
Mohammad <m.ghafari@fwutech.com>
2019-10-16 09:45:41 +0330

    Change to mci_management_report.sh bash script

--- mci_management_report.sh
+++ mci_management_report.sh
@@ -2,17 +2,16 @@
 cd /home/mohamad/Desktop/rpat_opt_scrips/Java_code/MciManagementReport
 
 export rpatMciMailServer="mail.fwutech.com"
-export rpatMciMailUsername="rpat-notification@fwutech.com"
-export rpatMciMailPassword="lbzTxgr2"
-export rpatMciMailRecipients="m.ghafari@fwutech.com, a.salari@fwutech.com, a.alaee@fwutech.com"
-# export rpatMciMailRecipients="a.alaee@fwutech.com, a.salari@fwutech.com, a.alaee@fwutech.com"
-export rpatMciDatabaseHuawei="jdbc:postgresql://10.186.86.5:5433/rpat"
-export rpatMciDatabaseUserHuawei="postgres"
-export rpatMciDatabasePasswordHuawei="123456"
+export rpatMciMailUsername=""
+export rpatMciMailPassword=""
+export rpatMciMailRecipients="m.example@example.com"
+export rpatMciDatabaseHuawei="jdbc:postgresql://sample_ip:5433/rpat"
+export rpatMciDatabaseUserHuawei="User"
+export rpatMciDatabasePasswordHuawei="Password"
 
-export rpatMciDatabaseNokia="jdbc:postgresql://10.186.86.4:5433/rpat"
-export rpatMciDatabaseUserNokia="postgres"
-export rpatMciDatabasePasswordNokia="123456"
+export rpatMciDatabaseNokia="jdbc:postgresql://sample_ip:5433/rpat"
+export rpatMciDatabaseUserNokia="user"
+export rpatMciDatabasePasswordNokia="password"
 
 
 /usr/bin/java -jar /home/mohamad/Desktop/rpat_opt_scrips/Java_code/MciManagementReport/target/MciManagementReport-0.0.1-SNAPSHOT-jar-with-dependencies.jar
